0795a2792a5cad9e80525287745d380a1a6ecbd2
andrew <asc269@cornell.edu>
2024-03-05 22:15:14 -0500

    formatting

--- grass/syscall.c
+++ grass/syscall.c
@@ -11,10 +11,11 @@
 #include "syscall.h"
 #include <string.h>
 
-static struct syscall *sc = (struct syscall*)SYSCALL_ARG;
+static struct syscall *sc = (struct syscall *)SYSCALL_ARG;
 
-static void sys_invoke() {
-    /* The standard way of system call is using the `ecall` instruction; 
+static void sys_invoke()
+{
+    /* The standard way of system call is using the `ecall` instruction;
      * Switching to ecall is given to students as an exercise */
     *((int *)0x2000000) = 1;
     asm("ecall");
@@ -23,28 +24,34 @@ static void sys_invoke() {
     // while (sc->type != SYS_UNUSED);
 }
 
-int sys_send(int receiver, char* msg, int size) {
-    if (size > SYSCALL_MSG_LEN) return -1;
+int sys_send(int receiver, char *msg, int size)
+{
+    if (size > SYSCALL_MSG_LEN)
+        return -1;
 
     sc->type = SYS_SEND;
     sc->msg.receiver = receiver;
     memcpy(sc->msg.content, msg, size);
     sys_invoke();
-    return sc->retval;    
+    return sc->retval;
 }
 
-int sys_recv(int* sender, char* buf, int size) {
-    if (size > SYSCALL_MSG_LEN) return -1;
+int sys_recv(int *sender, char *buf, int size)
+{
+    if (size > SYSCALL_MSG_LEN)
+        return -1;
 
     sc->type = SYS_RECV;
     sys_invoke();
     memcpy(buf, sc->msg.content, size);
-    if (sender) *sender = sc->msg.sender;
+    if (sender)
+        *sender = sc->msg.sender;
     return sc->retval;
 }
 
-void sys_exit(int status) {
+void sys_exit(int status)
+{
     struct proc_request req;
     req.type = PROC_EXIT;
-    sys_send(GPID_PROCESS, (void*)&req, sizeof(req));
+    sys_send(GPID_PROCESS, (void *)&req, sizeof(req));
 }
